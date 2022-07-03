6fa947ed44c8019f3ada3fe6b1c1d8109409c489
Nikki <62213020+nikkipanda-dev@users.noreply.github.com>
2022-07-03 21:00:53 +0800

    removes log message on enrollment update

--- src/components/widgets/StudentBio/index.js
+++ src/components/widgets/StudentBio/index.js
@@ -41,9 +41,6 @@ export const StudentBio = ({
 
     const status = values.is_enrolled ? "enrolled" : values.is_dropped ? "dropped" : values.is_expelled ? "expelled" : values.is_graduate ? "graduate" : '';
 
-    console.info('values ', values);
-    console.info('status ', status);
-
     if (!(enrollmentCategories[status])) {
         console.error('Invalid enrollment status.');
         return;
